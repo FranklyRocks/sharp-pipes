80eb87fff3f7830b180283b19a1e4fe4f798a2b6
bouldersharp <65699701+bouldersharp@users.noreply.github.com>
2022-02-20 00:53:54 +0100

    Update pipes.php

--- pipes.php
+++ pipes.php
@@ -1,12 +1,12 @@
 <?php
 class Pipe {
-    public static function new(...$args) : mixed {
+    public static function new(...$args) {
         $value = array_shift($args);
         foreach($args as $param) $value = $param($value);
         return $value;
     }
 }
 
-function pipe() : mixed {
+function pipe() {
     return Pipe::new(...func_get_args());
 }
