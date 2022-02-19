e89857738335b73d3ba244efbd5d7032cd849fff
bouldersharp <65699701+bouldersharp@users.noreply.github.com>
2022-02-20 00:18:40 +0100

    Add files via upload

---
+++ pipes.php
@@ -0,0 +1,13 @@
+<?php
+
+class Pipe {
+    public static function new(...$args) : mixed {
+        $value = array_shift($args);
+        foreach($args as $param) $value = $param($value);
+        return $value;
+    }
+}
+
+function pipe() : mixed {
+    return Pipe::new(...func_get_args());
+}
