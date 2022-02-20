de19d585363acbdf8cb95c77d771de170e4064f7
bouldersharp <65699701+bouldersharp@users.noreply.github.com>
2022-02-20 01:15:40 +0100

    Update pipes.min.php

--- pipes.min.php
+++ pipes.min.php
@@ -1,1 +1,1 @@
-<?php class Pipe{static function new(...$a) {$v=array_shift($a);foreach($a as $f)$v=$f($v);return $v;}}function pipe(){return Pipe::new(...func_get_args());}
+<?php class Pipe{static function new(...$a){$v=array_shift($a);foreach($a as $f)$v=$f($v);return $v;}}function pipe(){return Pipe::new(...func_get_args());}
